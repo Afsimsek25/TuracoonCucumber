fe0f21f00d2cfb9bd83cf1450cd07007893c5c25
Ahmet Furkan ŞİMŞEK <77547371+Afsimsek25@users.noreply.github.com>
2021-07-19 22:49:06 +0300

    runners added

--- src/test/java/runners/RegressionRunner.java
+++ src/test/java/runners/RegressionRunner.java
@@ -1,18 +1,34 @@
 package runners;
 
+import com.cucumber.listener.Reporter;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.AfterClass;
+
 
 @CucumberOptions(
 
+
         tags = {"@Regression"},
-        features = {"src/test/java/FeatureFiles"},
+        features = {"src/test/java/features"},
         glue = {"stepDefinitions"},
-        plugin = {"html:target/cucumber-report",
-                "json:target/cucumber.json"}
+        plugin = {"com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport/ExtentReportRegression.html"}
 
 
 )
 public class RegressionRunner extends AbstractTestNGCucumberTests {
+
+    @AfterClass
+    public static void afterClass() {
+        Reporter.loadXMLConfig("src/test/java/XMLFiles/ExtendReportSet.xml");
+
+        Reporter.setSystemInfo("User Name", "A.Furkan SIMSEK");
+        Reporter.setSystemInfo("Application Name", "Turacoon");
+        Reporter.setSystemInfo("Operating System Info", System.getProperty("os.name").toString());
+        Reporter.setSystemInfo("Department", "Product Development");
+        Reporter.setSystemInfo("Turacon Pre Release Sitesi", "Cucumber Test Otomasyonu");
+
+        Reporter.setTestRunnerOutput("Test Execution Cucumber Report");
+    }
 
 }
